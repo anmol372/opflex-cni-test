0fa004a5799645751e9c3879858d1bf43b54121f
Joji Mekkatt <jojimtt@gmail.com>
2019-04-22 13:59:50 -0700

    Changes to support running from mac

--- scripts/setup_registry.sh
+++ scripts/setup_registry.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# mac registry set up
+if [ "$(uname -s)" == "Darwin" ]
+then
+  docker run -d --name test_registry --restart=always -p 5000:5000 registry:2
+  exit 0
+fi
+
+# Linux
 #set private registry
 sudo echo '{ "insecure-registries":["1.100.201.1:5000"] }' | sudo tee /etc/docker/daemon.json
 sudo systemctl daemon-reload
