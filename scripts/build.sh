942cf073a7110c8117139a2b22889e6bf17b5d5c
Joji Mekkatt <jojimtt@gmail.com>
2019-08-16 15:49:18 -0700

    switch to master branch

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$SKIP_CLONE" ]
 then 
-  git clone https://github.com/noironetworks/aci-containers -b demo
+  git clone https://github.com/noironetworks/aci-containers
 fi
 cd aci-containers
 make go-build
