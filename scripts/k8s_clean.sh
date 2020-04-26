3e4f93ca686e9bddf2a0ba34720dff7cfa710e7e
Joji Mekkatt <jojimtt@gmail.com>
2020-04-25 21:28:24 -0700

    Fix clean up script

--- scripts/k8s_clean.sh
+++ scripts/k8s_clean.sh
@@ -8,14 +8,11 @@ kubectl --kubeconfig=data/kubeconfig delete svc hostnames-svc
 kubectl --kubeconfig=data/kubeconfig delete svc dns-test-svc
 kubectl --kubeconfig=data/kubeconfig delete epg epg-a epg-b -n kube-system
 kubectl --kubeconfig=data/kubeconfig delete contract tcp-6020 tcp-6021 -n kube-system
-#for pod in $(kubectl --kubeconfig=data/kubeconfig get pods | grep Run | awk '{print $1}')
-#do
-#  kubectl --kubeconfig=data/kubeconfig delete pod $pod
-#done
-#kubectl --kubeconfig=data/kubeconfig get pods 2> /dev/stdout | grep "No resources found"
-#while [[ $? -ne 0 ]]
-#do
-#    sleep 2
-#    kubectl --kubeconfig=data/kubeconfig get pods 2> /dev/stdout | grep "No resources found"
-#done
+for file in $(ls test/yamls/ext)
+do
+    kubectl --kubeconfig=data/kubeconfig delete -f test/yamls/ext/$file
+done
 kubectl --kubeconfig=data/kubeconfig get namespaces | grep "prod|dev"
+agentPod=$(kubectl --kubeconfig=data/kubeconfig get pods -n kube-system -o wide | grep "aci-containers-host" | grep test-node | awk '{print $1}')
+kubectl --kubeconfig=data/kubeconfig exec -it $agentPod -n kube-system -c aci-containers-host -- ifconfig veth_host 0.0.0.0
+kubectl --kubeconfig=data/kubeconfig exec -it $agentPod -n kube-system -c aci-containers-host -- ip route add 11.3.0.0/16 dev veth_host src 1.100.201.12
