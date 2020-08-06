fdfcd06335eb8e6a37f1bdf8d7934af6e83eb2dd
Joji Mekkatt <jojimtt@gmail.com>
2020-08-06 14:27:02 -0700

    Add a help text

--- scripts/run_it.sh
+++ scripts/run_it.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+
+#Usage: ../scripts/run_it.sh <acc_provision_input.yaml> <capic_admin_password>
 docker run --rm -e "KUBECONFIG=/opflex-cni-test/data/kubeconfig" -e PROV_INP_FILE=$1 -e PSWD=$2 -e "GW_IP=14.3.0.1" --net=host -v ${HOME}:${HOME} -w $(pwd)/it -it jojimt/gobuild pytest -v -s -x
